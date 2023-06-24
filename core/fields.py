743fe3611dcba46d9c54f97eb67d163b9ef9a606
el7futuro <el7futuro@gmail.com>
2023-06-24 11:46:51 +0500

    35.1 Core update

---
+++ core/fields.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.fields import CharField
+
+
+class PasswordField(CharField):
+    def __init__(self, **kwargs):
+        kwargs['style'] = {'input_type': 'password'}
+        kwargs['write_only'] = True
+        super().__init__(**kwargs)
+
+
+
